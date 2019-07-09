4ea5cae0fd5fb2011eb355752814fa77c00302eb
Leonid Terentyev <leonid.terentyev@webgears.at>
2019-07-09 12:40:51 +0300

    OP-112 moved php-fpm to separate image "wbgrs/php:7.2-node-fpm"

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ docker build . -f Dockerfile72 -t wbgrs/php:7.2
 docker build . -f Dockerfile70-node -t wbgrs/php:7.0-node
 docker build . -f Dockerfile71-node -t wbgrs/php:7.1-node
 docker build . -f Dockerfile72-node -t wbgrs/php:7.2-node
+docker build . -f Dockerfile72-node-fpm -t wbgrs/php:7.2-node-fpm
 
 docker push wbgrs/php:7.0
 docker push wbgrs/php:7.1
@@ -13,3 +14,4 @@ docker push wbgrs/php:7.2
 docker push wbgrs/php:7.0-node
 docker push wbgrs/php:7.1-node
 docker push wbgrs/php:7.2-node
+docker push wbgrs/php:7.2-node-fpm
