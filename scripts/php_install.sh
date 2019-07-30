34c99d362f5566f56decd7b89915d3df36dcd3c8
Leonid Terentyev <leonid.terentyev@webgears.at>
2019-07-30 09:46:07 +0300

    OP-112 added xsl and apcu php extension

--- scripts/php_install.sh
+++ scripts/php_install.sh
@@ -11,7 +11,7 @@ sed -i "s/\(memory_limit *= *\).*/\1-1/" $php_ini
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install apt-utils -yqq
-apt-get install git zip unzip zlib1g-dev libzip-dev wget libxml2-dev libpq-dev openssh-client sudo -yqq
+apt-get install git zip unzip zlib1g-dev libzip-dev wget libxml2-dev libpq-dev openssh-client sudo libxslt1-dev -yqq
 
 mkdir -p ~/.ssh && ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
@@ -24,4 +24,4 @@ docker-php-ext-enable pcov
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql pgsql intl zip
+docker-php-ext-install pdo_mysql pgsql intl zip xsl
