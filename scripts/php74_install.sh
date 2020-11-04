9cd947346c20bd411bd6a7afe3f8344c99036d47
gauss <kyryll.maesh@webgears-group.com>
2020-11-04 14:29:45 +0200

    COUP-7208 - composer 2

--- scripts/php74_install.sh
+++ scripts/php74_install.sh
@@ -19,9 +19,7 @@ apt install autoconf libtool -yqq
 mkdir -p ~/.ssh && ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
 # Install composer
-curl --silent --show-error https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-#Switch to composer 2
-composer self-update --2
+wget -cO - https://getcomposer.org/composer-stable.phar > /usr/local/bin/composer
 
 # Install fast composer package downloader
 composer global require hirak/prestissimo
