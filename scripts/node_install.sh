82dec1118b8ae9dfdfbccfe9bce61a276213a20f
Lukas Zimmermann <lukas.zimmermann@webgears-group.com>
2021-02-11 16:51:46 +0100

    Revert "COUP-7511 Refactored: moved refresh of keys from node installation to php installation"
    This reverts commit 51b22eb8

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -7,3 +7,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update && apt-get install -yqq gnupg
 apt-get install -yqq yarn
+apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com
