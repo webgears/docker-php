8803def9a17774cdb7dd764a1510398addb25ab8
Lukas Zimmermann <lukas.zimmermann@webgears-group.com>
2021-02-11 16:51:31 +0100

    Revert "COUP-7511 Refactored: moved installation of gnupg ro php_install files"
    This reverts commit 11159921

--- scripts/node_install.sh
+++ scripts/node_install.sh
@@ -5,4 +5,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -yqq gnupg
 apt-get install -yqq yarn
